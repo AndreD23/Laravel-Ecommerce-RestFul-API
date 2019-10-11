6c9626e33afdffb54cfc829d6a0cbc51d5456f1d
André Dorneles <andre.pereira@akna.com>
2019-10-11 13:40:49 -0300

    Criaçao de models com parametro -a, primeiros api endpoints criados

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::ApiResource('/products', 'ProductController');
+
+Route::group(['prefix' => 'products'], function(){
+    Route::ApiResource('/{product}/reviews', 'ReviewController');
+});
