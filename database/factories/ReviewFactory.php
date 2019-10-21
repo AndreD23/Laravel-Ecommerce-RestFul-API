02a04a5f4ace6f981970efb77241324545b8e4e8
André Dorneles <andre@imperiosoft.com.br>
2019-10-21 12:23:39 -0300

    Atualizando Factories
    - Atualização das factories para produtos e avaliações

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -2,11 +2,15 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Product;
 use App\Review;
 use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => Product::all()->random()->id,
+        'customer' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0, 5)
     ];
 });
