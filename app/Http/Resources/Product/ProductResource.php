5619dc70780bb90883cb77ac0dc5a698afd62463
André Dorneles <andre@imperiosoft.com.br>
2019-10-21 13:26:05 -0300

    Modificando detalhes do produto na api

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Foda de Estoque' : $this->stock,
+            'discount' => $this->discount,
+            'total_price' =>  round((1 - ($this->discount / 100)) * $this->price, 2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(), 2) : 'Não votado ainda',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
