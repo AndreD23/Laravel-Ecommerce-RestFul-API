fc6a2699941ae2876e02a7bb825d574aaa812065
André Dorneles <andre@imperiosoft.com.br>
2019-10-21 12:38:21 -0300

    Criando relacionamentos entre Produtos e Avaliações

--- app/Product.php
+++ app/Product.php
@@ -2,9 +2,14 @@
 
 namespace App;
 
+use App\Review;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
+
 }
--- app/Review.php
+++ app/Review.php
@@ -2,9 +2,15 @@
 
 namespace App;
 
+use App\Product;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+
 }
